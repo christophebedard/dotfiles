575ceecd23bc36e48d0ffda00be9da67f0b68bd1
christophebedard <chrisvolkoff@gmail.com>
2018-03-04 14:28:23 -0500

    Add basic install script

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Install useful packages
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install \
+    git \
+    python-catkin-tools \
+    yakuake
+
+sudo pip install \
+    powerline-shell
+
+# ROS
+. ./install_ros.sh
+
+
+# Update ~/.bashrc
+cat .bashrc >> ~/.bashrc
+
+# Update ~/.bash_aliases
+cat .bash_aliases >> ~/.bash_aliases
