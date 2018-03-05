0212512c621497097513af9fb97da5bd10031b6c
christophebedard <chrisvolkoff@gmail.com>
2018-03-05 10:53:39 -0500

    Add line to get out of tmp/ directory after install

--- install.sh
+++ install.sh
@@ -25,6 +25,7 @@ cd tmp/
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt-get -f install -y
+cd ../
 
 # code
 
