975e0dbe647b7d7ddb01768a0764266c5284a513
Christophe Bedard <bedard.christophe@gmail.com>
2021-06-01 16:08:40 -0400

    Disable 'click to minimize' because it's a pain with multiple windows

--- set_gsettings.sh
+++ set_gsettings.sh
@@ -17,4 +17,4 @@ gsettings set org.gnome.shell.extensions.desktop-icons show-home false
 gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts false
 
 # minimize on click
-gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
+#gsettings set org.gnome.shell.extensions.dash-to-dock click-action 'minimize'
